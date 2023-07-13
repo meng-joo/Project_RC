38002f63af767abef8ce87e43d162f4bf1c7bf30
hanmin0728 <gksalsdud1234@gmail.com>
2023-07-13 16:39:30 +0900

    풀매니저 추가 및 맵 구조

---
+++ Assets/01.Script/Min/Core/MapGenerator.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+   private List<MapSO> mapKinList = new List<MapSO>();
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/01.Script/Min/SO/ChaterSO.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="SO/Chater")]
+public class ChaterSO : ScriptableObject
+{
+    public int mapCnt;
+
+    public int battleMapCnt;
+    public int eventMapCnt;
+    public int shopMapCnt;
+}
